11611216c049ee467a7d3c0374ffdcebac07ea87
Wang <eeewl1250@163.com>
2018-10-31 20:42:36 +0800

    adjust the layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <el-button type="primary" plain v-on:click="$router.push('/')">Home</el-button>
+    <span v-if="user">
+      User: {{ user }}
+      <el-button type="warning" v-on:click="logout">Logout</el-button>
+    </span>
+    <el-button v-else type="success" v-on:click="login">Login</el-button>
     <hr />
     <router-view/>
   </div>
@@ -8,7 +13,24 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    login () {
+      this.$router.replace('/login')
+    },
+    logout () {
+      this.$store.dispatch('logout')
+        .then(() => {
+          this.$cookies.remove(this.$store.state.cookieName)
+          this.$router.replace('/login')
+        })
+    }
+  },
+  computed: {
+    user () {
+      return this.$store.state.user
+    }
+  }
 }
 </script>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,51 +1,15 @@
 <template>
   <div>
     <h2>Main Page</h2>
-    <span v-if="user">
-      User: {{ user }}
-      <el-button type="warning" v-on:click="logout">Logout</el-button>
-    </span>
-    <el-button v-else type="success" v-on:click="login">Login</el-button>
     <p>
-      <el-button type="primary" plain v-on:click="wordsImport">Words Import</el-button>
-    </p>
-    <p>
-      <el-button type="primary" plain v-on:click="test">Cookie test</el-button>
+      <el-button type="primary" plain v-on:click="$router.push('/import')">Words Import</el-button>
     </p>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Main',
-  methods: {
-    login () {
-      this.$router.replace('/login')
-    },
-    logout () {
-      this.$store.dispatch('logout')
-        .then(() => {
-          this.$cookies.remove(this.$store.state.cookieName)
-          this.$router.replace('/login')
-        })
-    },
-    wordsImport () {
-      this.$router.push('/import')
-    },
-    test () {
-      this.$cookie.set('username', 'bucky')
-
-      console.log(this.$cookie.get())
-      console.log(document.cookie)
-      document.cookie = encodeURIComponent('name') + '=' + encodeURIComponent('username')
-    }
-  },
-  computed: {
-    user () {
-      return this.$store.state.user
-    }
-  }
+  name: 'Main'
 }
 </script>
 
